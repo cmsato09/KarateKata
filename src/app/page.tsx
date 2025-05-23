ed5ae1bc79fd43e7b479070829ec57fa120af5d3
Chris Sato <cmsato09.git@gmail.com>
2025-05-22 17:28:23 -0700

    Fix className typo for link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
     <div className="min-h-screen flex flex-col items-center justify-center -mt-16">
       <Link 
         href="/techniques"
-        className="text-lg bg-blue-500 hover:g-blue-600 text-white"
+        className="text-lg bg-blue-500 hover:bg-blue-600 text-white"
       >
         View Techniques
       </Link>
