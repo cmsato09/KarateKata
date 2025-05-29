3c50384565e2622759ef60591cd0e68267792127
Chris Sato <cmsato09.git@gmail.com>
2025-05-28 17:28:53 -0700

    Remove rudimentary table using HTML <table>

--- src/app/techniques/page.tsx
+++ src/app/techniques/page.tsx
@@ -7,25 +7,6 @@ export default async function TechniqueList() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8">Techniques</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Japanese</th>
-          </tr>
-        </thead>
-        <tbody>
-          {techniques.map((technique) => (
-            <tr key={technique.id}>
-              <td>{technique.name}</td>
-              <td>{technique.type}</td>
-              <td>{technique.name_kanji} -- {technique.name_hiragana}</td>
-            </tr>
-          ))}
-          
-        </tbody>
-      </table>
       <DataTable columns={columns} data={techniques}/>
 
     </div>
