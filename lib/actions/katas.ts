1dfb612aaf9099badc527b84e395542af31d7da2
Chris Sato <cmsato09.git@gmail.com>
2026-01-29 22:37:43 -0800

    Add createKata server action and form handler
    - Also fixed typos

--- lib/actions/katas.ts
+++ lib/actions/katas.ts
@@ -1,4 +1,4 @@
-"user server"
+"use server"
 
 import { revalidatePath } from "next/cache";
 import type { Kata } from "@/app/generated/prisma/browser";
@@ -12,6 +12,65 @@ export async function getKatas() {
     })
   } catch (error) {
     console.error("Database Error:", error);
-    throw new Error("Failed to load stance data");
+    throw new Error("Failed to load kata data");
   }
+}
+
+export async function createKata(formData: FormData) {
+  try {
+    const validatedKataData = validateKataData(formData);
+
+    const existingKata = await prisma.kata.findUnique({
+      where: {
+        style_name: {
+          style: validatedKataData.style,
+          name: validatedKataData.name,
+        },
+      },
+    });
+    if (existingKata) {
+      throw new Error(`Kata "${validatedKataData.name}" already exists`);
+    }
+
+    const kata = await prisma.kata.create({
+      data: validatedKataData,
+    });
+
+    return kata;
+  } catch (error) {
+    console.error("Database error", error);
+    if (error instanceof Error) {
+      throw error;
+    }
+    throw new Error("Could not create new kata. Check inputs and try again.");
+  }
+}
+
+export async function createKataAction(
+  prevState: {
+    success: boolean;
+    error: string | null;
+    kata: Kata | null;
+    operation?: string;
+  } | null,
+  formData: FormData,
+) {
+  try {
+    const kata = await createKata(formData);
+    revalidatePath("/katas");
+    return {
+      success: true,
+      error: null,
+      kata,
+      operation: "create",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error instanceof Error ? error.message: "Failed to create kata",
+      kata: null,
+      operation: "create",
+    };
+  }
+
 }
